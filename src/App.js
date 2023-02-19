b3a03970adf4c130cdaea16e8bc62a107a549977
sandy_gaonkar@yahoo.com <sandy_gaonkar@yahoo.com>
2023-02-19 21:28:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        
+      fetch(`${process.env.REACT_APP_API_KEY}`,    
+      {
+      method: "GET",
+      headers: {
+        "access-control-allow-origin" : "*",
+        "Content-type": "application/json; charset=UTF-8"
+      }})  
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Please wait some time.... </h1> </div> ;
+   
+        return (
+        <div className = "App">
+            <h1> AHM Users </h1>  {
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    First Name: { item.firstName }, 
+                    Last Name: { item.lastName }
+                    </ol>
+                ))
+            }
+        </div>
+    );
 }
-
+}
+   
 export default App;
